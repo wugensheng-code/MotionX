1f8ae49b1412a5133278a1b436f2633d0c2c2379
王宸宇 <wangchenyu@rt-thread.com>
2021-07-14 20:17:39 +0800

    BSP

---
+++ chip/Inc/main.h
@@ -0,0 +1,88 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define IMU_SCL_Pin GPIO_PIN_8
+#define IMU_SCL_GPIO_Port GPIOB
+#define IMU_SDA_Pin GPIO_PIN_9
+#define IMU_SDA_GPIO_Port GPIOB
+#define LED_R_R_Pin GPIO_PIN_14
+#define LED_R_R_GPIO_Port GPIOC
+#define LED_G_R_Pin GPIO_PIN_13
+#define LED_G_R_GPIO_Port GPIOC
+#define LED_B_L_Pin GPIO_PIN_12
+#define LED_B_L_GPIO_Port GPIOB
+#define LED_R_L_Pin GPIO_PIN_7
+#define LED_R_L_GPIO_Port GPIOA
+#define LED_G_L_Pin GPIO_PIN_6
+#define LED_G_L_GPIO_Port GPIOA
+#define MPU_INT_Pin GPIO_PIN_4
+#define MPU_INT_GPIO_Port GPIOA
+#define MPU_INT_EXTI_IRQn EXTI4_IRQn
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
